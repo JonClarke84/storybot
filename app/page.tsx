27cf9a3a03df6f876eb559b32b553ad53db3ca26
Jon Clarke <jonathan.clarke@holidayextras.com>
2022-12-08 12:08:26 +0000

    init

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to StoryBot!
+        </h1>
+
+        <textarea className={styles.textarea} placeholder="Enter your story prompt here" />
+
+        <button className={styles.button}>Generate Story</button>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
