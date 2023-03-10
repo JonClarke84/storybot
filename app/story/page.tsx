7d59edc68e3cd2ccd2c8d5629d98a0da6802deac
Jon Clarke <jonathan.clarke@holidayextras.com>
2023-03-10 14:56:16 +0000

    test

--- app/story/page.tsx
+++ app/story/page.tsx
@@ -2,9 +2,15 @@ import Image from 'next/image'
 import { Story, PromptDetails } from '../types/types'
 import getStory from '../lib/getStory'
 
-export default async function StorySection({ searchParams } : { searchParams: any }): Promise<JSX.Element> {
+export default async function StorySection({ searchParams }: any): Promise<JSX.Element> {
   const prompt: PromptDetails = searchParams || ''
-  const data: Story = prompt && await getStory(prompt)
+  let data = {
+    story: '',
+    imageUrl: ''
+  }
+  if (prompt) {
+    data = await getStory(prompt)
+  }
   const { story: generatedStory } = data
 
   return (
